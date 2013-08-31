ba26a43652ae58248ef1aca13db5ef1e26102654
David Souther <davidsouther@gmail.com>
2013-08-31 16:21:26 -0400

    expand/collapse

--- scripts.js
+++ scripts.js
@@ -6,6 +6,24 @@ function trimLeadingTabs(){
 	[].forEach.call(pres, trimTabs);
 }
 
+function accordinate(){
+	var css = "minify";
+	var h3s = document.getElementsByTagName("h3");
+
+	var toggle = function(event){
+		classes = event.target.parentNode.classList;
+		var fn = classes.contains(css) ? "remove" : "add"
+		classes[fn](css);
+	}
+
+	var bind = function(el){
+		el.onclick = toggle;
+	};
+
+	[].forEach.call(h3s, bind);
+}
+
 window.onload = function(){
 	trimLeadingTabs();
-}
+	accordinate();
+};
